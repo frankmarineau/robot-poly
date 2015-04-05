c1a61e436da209418a2a722801e162eabab26d4e
Francis Marineau <frank.marineau@gmail.com>
2015-04-04 20:51:49 -0400

    Fini la classe ports

--- lib_dir/Ports.cpp
+++ lib_dir/Ports.cpp
@@ -1,41 +1,47 @@
 #include "Ports.h"
 
-Ports::initialiserPorts(){
-
+Ports::initialiserPorts() {
+	DDRA = 0x00; // Capteur de ligne et photoresistances (entrées)
+	DDRB = 0xff; // Buzzer et sonar (sortie)
+	DDRC = 0xff; // LCD (sortie)
+	DDRD = 0b11000000; // Boutons (entrée) et moteurs (sortie)
 }
 
-uint8_t Ports::lireSonar(){
+uint8_t Ports::lireSonar() {
 	return PINB0;
 }
 
-uint8_t Ports::lirePhotoresistanceGauche(){
-	return (PINA1 << 1) | PINA0;
-	// TODO: Lire avec can
+uint8_t Ports::lirePhotoresistanceGauche() {
+	return this.capteur.lecture(0) >> 2;
 }
 
-uint8_t Ports::lirePhotoresistanceDroite(){
-	return (PINA1 << 1) | PINA0;
-	// TODO: Lire avec can
+uint8_t Ports::lirePhotoresistanceDroite() {
+	return this.capteur.lecture(1) >> 2;
 }
 
-uint8_t Ports::lireCapteur(){
+uint8_t Ports::lireCapteur() {
 	return (PINA7 << 5) | (PINA6 << 4) | (PINA5 << 3) | (PINA4 << 2) | (PINA3 << 1) | PINA2;
 }
 
-uint8_t Ports::lireBoutonSelection(){
+uint8_t Ports::lireBoutonSelection() {
 	return PIND0;
 }
 
-uint8_t Ports::lireBoutonConfirmation(){
+uint8_t Ports::lireBoutonConfirmation() {
 	return PIND1;
 }
 
-void Ports::setDirectionMoteur(uint8_t val){
+void Ports::setDirectionMoteur(uint8_t val) {
 	PORTD &= 0b11110011;
 	PORTD |= val << 2;
 }
 
-void Ports::setDirectionMoteur(uint8_t val){
-	PORTD &= 0b11110011;
-	PORTD |= val << 2;
+void Ports::setSon(uint8_t val) {
+	PORTB &= 0b11110011;
+	PORTB |= val << 2;
+}
+
+void Ports::setSonar(uint8_t val) {
+	PORTD &= 0b11111100;
+	PORTD |= val;
 }
--- lib_dir/Ports.h
+++ lib_dir/Ports.h
@@ -1,4 +1,4 @@
-#ifndef PORTS_Hvolatile uint8_t *port
+#ifndef PORTS_H
 #define PORTS_H
 
 class Ports {
@@ -7,7 +7,8 @@ public:
 
 	//Input
 	static uint8_t lireSonar();
-	static uint8_t lirePhotoresistance();
+	static uint8_t lirePhotoresistanceGauche();
+	static uint8_t lirePhotoresistanceDroite();
 	static uint8_t lireCapteur();
 	static uint8_t lireBoutonSelection();
 	static uint8_t lireBoutonConfirmation();
@@ -16,6 +17,9 @@ public:
 	static void setDirectionMoteur(uint8_t val);
 	static void setSon(uint8_t val);
 	static void setSonar(uint8_t val);
+
+private:
+	static can capteur;
 };
 
 #endif
