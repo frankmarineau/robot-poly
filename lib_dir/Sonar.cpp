95be28cf7b64ecdd528297e4e4ce868151feb703
foxtrotDS0x01 <foromodanielsoromou@gmail.com>
2015-04-13 16:22:39 -0400

    propagation des modifications

--- lib_dir/Sonar.cpp
+++ lib_dir/Sonar.cpp
@@ -1,3 +1,23 @@
 #include "Sonar.h"
 
+Sonar::Sonar()
+{
+
+}
+
+uint8_t Sonar::getDistance()
+{
+
+}
+
+void Sonar::setDistance(uint8_t d)
+{
+
+}
+
+
+uint8_t Sonar::readDistance()
+{
+
+}
 
--- lib_dir/Sonar.h
+++ lib_dir/Sonar.h
@@ -4,16 +4,16 @@
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
-/*
+
 class Sonar{
 
    private:
    	uint8_t distance_;
    public:
      Sonar();    
-     uint8_t getDistance;
-     void setDistance(uint8_t d) 
+     uint8_t getDistance();
+     void setDistance(uint8_t d) ;
      uint8_t readDistance();
-};*/
+};
 
 #endif
